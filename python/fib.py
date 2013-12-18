4448fd4bf19d2201e7ae011e0162567212bb9c8e
ingemar <ingemar@inet.(none)>
2013-12-18 23:09:06 +0100

    Improvement

--- python/fib.py
+++ python/fib.py
@@ -1,6 +1,19 @@
+#!/usr/bin/python
+
+import sys
+#print "This is the name of the script: ", sys.argv[0]
+#print "Number of arguments: ", len(sys.argv)
+#print "The arguments are: " , str(sys.argv)
+
+if len(sys.argv) != 2:
+  print "Usage %s n" % sys.argv[0]
+  exit(1)
+
 def fib(n):
   if n<=2:
     return 1
   return fib(n-1) + fib(n-2)
 
-print fib(20)
+i=int(sys.argv[1])
+
+print "fib(%d) = %d" % (i, fib(i))
