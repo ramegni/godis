3f4685f41df2f7b9e7ff741af78600ed7c31c5fa
Ingemar Assarsjo <ingemar.assarsjo@ericsson.com>
2015-09-24 09:29:04 +0200

    Improvements
    Klarar nu alla fyra räknesätten.
    Printar nu utan onödiga paranteser.

--- exp/exp.py
+++ exp/exp.py
@@ -1,6 +1,21 @@
+# need_par state if parentheses needed around the left resp. right side of the operator op
+# in the printout of the expression "(e1 op-left e2) op (e3 op-right e4)"
+# need_par_left[
+need_par_left  = {('*','+'): 1,
+                  ('*','-'): 1,
+                  ('/','+'): 1,
+                  ('/','-'): 1,
+                 }
+need_par_right = {('-','+'): 1,
+                  ('-','-'): 1,
+                  ('*','+'): 1,
+                  ('*','-'): 1,
+                  ('/','+'): 1,
+                  ('/','-'): 1,
+                 }
+
 class Exp():
 
-    value = ()
     def __init__(self, e):
         self.typ = "leaf"
         self.value = e
@@ -13,20 +28,51 @@ class Exp():
         e.typ = "binop"
         return(e)
 
+    def __sub__(self, other):
+        e = Exp(0)
+        e.left = self
+        e.right= other
+        e.op = '-'
+        e.typ = "binop"
+        return(e)
+
+    def __mul__(self, other):
+        e = Exp(0)
+        e.left = self
+        e.right= other
+        e.op = '*'
+        e.typ = "binop"
+        return(e)
+
+    def __div__(self, other):
+        e = Exp(0)
+        e.left = self
+        e.right= other
+        e.op = '/'
+        e.typ = "binop"
+        return(e)
+
     def __str__(self):
         if self.typ == "leaf":
             return self.value.__str__()
         if self.typ == "binop":
-            return "(%s)%s(%s)" % (self.left.__str__(), self.op, self.right.__str__())
+            l = self.left.__str__()
+            if self.left.typ == "binop":
+                if (self.op, self.left.op) in need_par_left:
+                    l = '(' + l + ')'
+            r = self.right.__str__()
+            if self.right.typ == "binop":
+                if (self.op, self.right.op) in need_par_right:
+                    r = '(' + r + ')'
+            return "%s%s%s" % (l, self.op, r)
 
 e1 = Exp('x')
 e2 = Exp(5)
-
-e3 = e1 + e2;
-
+e3 = e1 + e2
+e4 = e1 * e3
+e5 = e3 - e4
 print("e1 = %s" % e1)
 print("e2 = %s" % e2)
 print("e3 = %s" % e3)
-
-e4 = e1 + 7
 print("e4 = %s" % e4)
+print("e5 = %s" % e5)
